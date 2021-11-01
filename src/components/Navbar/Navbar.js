572c7ba07924b725b0e52402240754743e6c1fab
gleb <gyrra98@mail.ru>
2021-11-01 23:10:34 +0200

    use activeClassName in NavLink

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,24 +1,24 @@
 import React from "react"
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 import classes from './Navbar.module.css'
 
 const Navbar = () => {
     return (
         <nav className={classes.nav}>
-            <div className={`${classes.item} ${classes.active}`}>
-                <Link to='/profile'>Profile</Link>
+            <div className={`${classes.item}`}>
+                <NavLink to='/profile' activeClassName={classes.activeLink}>Profile</NavLink>
             </div>
             <div className={classes.item}>
-                <Link to='/messages'>Messages</Link>
+                <NavLink to='/messages' activeClassName={classes.activeLink}>Messages</NavLink>
             </div>
             <div className={classes.item}>
-                <Link to='/news'>News</Link>
+                <NavLink to='/news' activeClassName={classes.activeLink}>News</NavLink>
             </div>
             <div className={classes.item}>
-                <Link to='/music'>Music</Link>
+                <NavLink to='/music' activeClassName={classes.activeLink}>Music</NavLink>
             </div>
             <div className={classes.item}>
-                <Link to='/settings'>Settings</Link>
+                <NavLink to='/settings' activeClassName={classes.activeLink}>Settings</NavLink>
             </div>
         </nav>
     )
