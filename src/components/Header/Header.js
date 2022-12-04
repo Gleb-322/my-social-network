d39d14ba1d5b5ef93aa3b048e9428cb768f2bb72
slin4er <slin4eria@gmail.com>
2022-12-04 14:54:04 +0100

    connection check

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,6 +5,7 @@ const Header = () => {
     return (
         <header className={classes.header}>
             <img src='https://crosti.ru/patterns/00/0c/fc/2581e6a797/picture.jpg' alt='logo'/>
+            <h1>check for git connection</h1>
         </header>
     )
 }
